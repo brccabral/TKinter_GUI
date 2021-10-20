fe41bafc11aa60e9cba503ae6f580890eb4e2280
Bruno Cabral <cabral.brc@gmail.com>
2021-10-20 12:48:33 -0700

    buttons 101

---
+++ buttons.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+from tkinter.constants import DISABLED
+
+root = tk.Tk()
+root.geometry("400x400")
+root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
+
+def myClick():
+    myLabel = tk.Label(root, text="You clicked")
+    myLabel.pack()
+
+myButton = tk.Button(root, text="Click me!")
+myButton.pack()
+myButton2 = tk.Button(root, text="Can't click me!", state=DISABLED)
+myButton2.pack()
+
+myButton3 = tk.Button(root, text="Click me!", padx=50, pady=25, command=myClick)
+myButton3.pack()
+myButton4 = tk.Button(root, text="Click me!", padx=50, pady=25, foreground="blue", background="green", highlightbackground="yellow", highlightcolor="white")
+myButton4.pack()
+myButton5 = tk.Button(root, text="Click me!", padx=50, pady=25, fg="white", bg="red", highlightbackground="magenta", highlightcolor="#888888")
+myButton5.pack()
+
+root.mainloop()
--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import tkinter as tk
 
 root = tk.Tk()
 root.geometry("400x400")
+root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 
 # Creating a label Widget
 myLabel = tk.Label(root, text="Hello World!")
