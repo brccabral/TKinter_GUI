32fcfb1ee68d1dc5aa27a9a7961580436080d4f3
brccabral <cabral.brc@gmail.com>
2021-10-21 00:32:01 -0700

    databases create .db and a table

---
+++ create_database.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+import os
+import sqlite3
+
+root = tk.Tk()
+root.title("TKinter GUI")
+if os.name == "nt":
+    root.wm_iconbitmap(bitmap="python3.ico")
+else:
+    root.wm_iconbitmap(bitmap="@python3.xbm")
+root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
+root.geometry("400x400")
+
+conn = sqlite3.connect("address_book.db")
+
+c = conn.cursor()
+c.execute("""CREATE TABLE addresses(
+    first_name text,
+    last_name text,
+    address text,
+    city text,
+    state text,
+    zipcode integer
+)
+""")
+
+
+conn.commit()
+conn.close()
+
+root.mainloop()
