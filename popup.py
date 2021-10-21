d84eb8620c0dcb571672156409fe386a6f8e7722
brccabral <cabral.brc@gmail.com>
2021-10-20 23:08:33 -0700

    more popups, ok cancel, yes, no

--- popup.py
+++ popup.py
@@ -21,8 +21,33 @@ def popup_warn():
 def popup_error():
     messagebox.showerror("This is my popup", "Hello")
 
+def popup_askquestion():
+    # yes or no
+    messagebox.askquestion("Question", "Go forward?")
+
+def popup_askokcancel():
+    # ok or cancel
+    messagebox.askokcancel("Question", "Accept change?")
+
+def popup_askretrycancel():
+    # retry or cancel
+    messagebox.askretrycancel("Question", "Can't open")
+
+def popup_askyesno():
+    # yes or no
+    messagebox.askyesno("Question", "Go left?")
+
+def popup_askyesnocancel():
+    # yes or no or cancel
+    messagebox.askyesnocancel("Question", "Yes, no or cancel?")
+
 Button(root, text="Info", command=popup_info).pack()
 Button(root, text="Warning", command=popup_warn).pack()
 Button(root, text="Error", command=popup_error).pack()
+Button(root, text="Ask question", command=popup_askquestion).pack()
+Button(root, text="Ask ok or cancel", command=popup_askokcancel).pack()
+Button(root, text="Ask retry or cancel", command=popup_askretrycancel).pack()
+Button(root, text="Ask yes or no", command=popup_askyesno).pack()
+Button(root, text="Ask yes or no or cancel", command=popup_askyesnocancel).pack()
 
 root.mainloop()
