56f61f31307d1567e5ac896cd2710a64f5443eae
brccabral <cabral.brc@gmail.com>
2021-10-20 22:53:06 -0700

    use string in radio buttons

--- radio_button.py
+++ radio_button.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import os
+from tkinter.constants import W
 
 root = tk.Tk()
 root.title("TKinter GUI")
@@ -10,22 +11,35 @@ else:
 root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 # root.geometry("400x400")
 
-r = tk.IntVar()
-r.set(2) # default value, Option 2 is selected at start
+# r = tk.IntVar()
+# r.set(2) # default value, Option 2 is selected at start
 
 # command is run at each time an option is selected, 
 # even if the option is already selected
-tk.Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
-tk.Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
+# tk.Radiobutton(root, text="Option 1", variable=r, value=1, command=lambda: clicked(r.get())).pack()
+# tk.Radiobutton(root, text="Option 2", variable=r, value=2, command=lambda: clicked(r.get())).pack()
 
-label_1 = tk.Label(root, text=r.get())
+TOPPINGS = [
+    ('Pepperoni', "Pepperoni"),
+    ('Cheese', "Cheese"),
+    ('Mushroom', "Mushroom"),
+    ('Onion', "Onion")
+]
+
+pizza = tk.StringVar()
+pizza.set("Pepperoni")
+
+for topping_name, topping in TOPPINGS:
+    tk.Radiobutton(root, text=topping_name, variable=pizza, value=topping, command=lambda: clicked(pizza.get())).pack(anchor=W)
+
+label_1 = tk.Label(root, text=pizza.get())
 label_1.pack()
 
 def clicked(value):
     label_1 = tk.Label(root, text=value)
     label_1.pack()
 
-my_button = tk.Button(root, text="Click me", command=lambda: clicked(r.get()))
+my_button = tk.Button(root, text="Click me", command=lambda: clicked(pizza.get()))
 my_button.pack()
 
 root.mainloop()
