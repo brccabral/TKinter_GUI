bbd9240775dee104ae4da9d60851f0591a53cee0
brccabral <cabral.brc@gmail.com>
2021-10-21 00:16:13 -0700

    use checkbox with custom values

--- checkbox.py
+++ checkbox.py
@@ -11,11 +11,15 @@ root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 root.geometry("400x400")
 
 value = tk.IntVar()
-c = tk.Checkbutton(root, text="Check here", variable=value)
+str_value = tk.StringVar()
+# default values are 0 and 1
+# use onvalue and offvalue to set custom values
+c = tk.Checkbutton(root, text="Check here", variable=str_value, onvalue="sim", offvalue="nao")
+c.deselect()
 c.pack()
 
 def show_value():
-    myLabel = tk.Label(root, text=value.get())
+    myLabel = tk.Label(root, text=str_value.get())
     myLabel.pack()
 
 myButton = tk.Button(root, text="Show selection", command=show_value)
