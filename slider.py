cc06d2ccc8c8a2661cfdba1d58ba69dc2a376d2e
brccabral <cabral.brc@gmail.com>
2021-10-21 00:05:40 -0700

    change both dimensions dynamic

--- slider.py
+++ slider.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-from tkinter.constants import HORIZONTAL
+from tkinter.constants import HORIZONTAL, W
 
 root = tk.Tk()
 root.title("TKinter GUI")
@@ -12,14 +12,14 @@ root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 root.geometry("400x400")
 
 def dynamic_size(value):
-    my_horizontal = tk.Label(root, text=value).pack()
-    root.geometry(str("400x")+value)
+    tk.Label(root, text=value).pack()
+    root.geometry(f"{horizontal.get()}x{vertical.get()}")
 
 vertical = tk.Scale(root, from_=200, to=500, command=dynamic_size)
 vertical.pack()
 
-horizontal = tk.Scale(root, from_=200, to=400, orient=HORIZONTAL)
-horizontal.pack()
+horizontal = tk.Scale(root, from_=200, to=400, orient=HORIZONTAL, command=dynamic_size)
+horizontal.pack(anchor=W)
 
 def show_hor():
     my_horizontal = tk.Label(root, text=horizontal.get()).pack()
