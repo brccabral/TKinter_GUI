546ee4a1fc1cb73ff11f514f85014d5415070d0a
brccabral <cabral.brc@gmail.com>
2021-10-20 23:36:30 -0700

    new window after button click

--- new_window.py
+++ new_window.py
@@ -6,18 +6,29 @@ root = tk.Tk()
 root.title("TKinter GUI Window")
 root.geometry("400x400")
 
-top = tk.Toplevel()
-top.title("New window")
-if os.name == "nt":
-    top.wm_iconbitmap(bitmap="python3.ico")
-else:
-    top.wm_iconbitmap(bitmap="@python3.xbm")
-top.iconphoto(top._w, tk.PhotoImage(file='python3.png'))
+image = 0
+def open_window():
+    # image needs to be global for python garbage collection
+    # doesn't delete this
+    global image
+    top = tk.Toplevel()
+    top.title("New window")
+    if os.name == "nt":
+        top.wm_iconbitmap(bitmap="python3.ico")
+    else:
+        top.wm_iconbitmap(bitmap="@python3.xbm")
+    top.iconphoto(top._w, tk.PhotoImage(file='python3.png'))
 
-label = tk.Label(top, text="Hello top").pack()
+    label = tk.Label(top, text="Hello top")
+    label.pack()
 
-image = ImageTk.PhotoImage(Image.open("images/0.jpg"))
-img_holder = tk.Label(top, image=image).pack()
+    image = ImageTk.PhotoImage(Image.open("images/0.jpg"))
+    img_holder = tk.Label(top, image=image)
+    img_holder.pack()
 
+
+
+button = tk.Button(root, text="Open new window", command=open_window)
+button.pack()
 
 root.mainloop()
