62aaf116fabd664608c1e4f3f4e37a3c492b4c60
brccabral <cabral.brc@gmail.com>
2021-10-20 23:31:33 -0700

    new window opens on run

---
+++ new_window.py
@@ -0,0 +1,23 @@
+import tkinter as tk
+import os
+from PIL import ImageTk, Image
+
+root = tk.Tk()
+root.title("TKinter GUI Window")
+root.geometry("400x400")
+
+top = tk.Toplevel()
+top.title("New window")
+if os.name == "nt":
+    top.wm_iconbitmap(bitmap="python3.ico")
+else:
+    top.wm_iconbitmap(bitmap="@python3.xbm")
+top.iconphoto(top._w, tk.PhotoImage(file='python3.png'))
+
+label = tk.Label(top, text="Hello top").pack()
+
+image = ImageTk.PhotoImage(Image.open("images/0.jpg"))
+img_holder = tk.Label(top, image=image).pack()
+
+
+root.mainloop()
