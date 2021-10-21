5e666087724b6e81e4ce71c30c174b32a30a8f6b
Bruno Cabral <cabral.brc@gmail.com>
2021-10-21 13:19:23 -0700

    correct file name

--- plot_matplotlib.py
+++ plot_matplotlib.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import os
+import numpy as np
+import matplotlib.pyplot as plt
 
 root = tk.Tk()
 root.title("TKinter GUI")
@@ -8,8 +10,14 @@ if os.name == "nt":
 else:
     root.wm_iconbitmap(bitmap="@python3.xbm")
 root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
-root.geometry("400x400")
+root.geometry("400x200")
 
+def graph():
+    house_prices = np.random.normal(200_000, 25_000, 5_000)
+    plt.hist(house_prices, 50)
+    plt.show()
 
+button = tk.Button(root, text="Graph it", command=graph)
+button.pack()
 
 root.mainloop()
