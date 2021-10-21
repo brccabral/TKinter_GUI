b40f0b046a514beffe3cf3341363cdbfe63716d1
brccabral <cabral.brc@gmail.com>
2021-10-20 23:50:56 -0700

    button opens file dialog, replace image using grid

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-from tkinter import Label, filedialog
+from tkinter import Button, Label, filedialog
 from PIL import ImageTk, Image
 
 root = tk.Tk()
@@ -12,13 +12,19 @@ else:
 root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 root.geometry("400x400")
 
+my_image = 0
+def open_file():
+    global my_image
+    root.filename = filedialog.askopenfilename(initialdir="images", title="Select a file", filetypes=(("png files","*.png"),("all files", "*.*")))
 
-root.filename = filedialog.askopenfilename(initialdir="images", title="Select a file", filetypes=(("png files","*.png"),("all files", "*.*")))
+    my_label = Label(root, text=root.filename)
+    my_label.grid(row=1, column=0)
 
-my_label = Label(root, text=root.filename).pack()
+    my_image = ImageTk.PhotoImage(Image.open(root.filename))
+    my_image_label = Label(image=my_image)
+    my_image_label.grid(row=2, column=0)
 
-my_image = ImageTk.PhotoImage(Image.open(root.filename))
-my_image_label = Label(image=my_image).pack()
-
+button = Button(root, text="Open file", command=open_file)
+button.grid(row=0, column=0)
 
 root.mainloop()
