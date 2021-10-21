7339e142d5e89e60d473b73d9358771611c9f3d2
brccabral <cabral.brc@gmail.com>
2021-10-20 22:37:26 -0700

    play with pad inside or outside

--- frames.py
+++ frames.py
@@ -8,9 +8,11 @@ if os.name == "nt":
 else:
     root.wm_iconbitmap(bitmap="@python3.xbm")
 root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
-root.geometry("400x400")
+# root.geometry("400x400")
 
-frame = tk.LabelFrame(root, text="This is my frame", padx=5, pady=5)
+# pad inside
+frame = tk.LabelFrame(root, text="This is my frame", padx=50, pady=50)
+# pad outside
 frame.pack(padx=10, pady=10)
 
 b = tk.Button(frame, text="Click me")
