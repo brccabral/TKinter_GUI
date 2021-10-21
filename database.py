84b41b51b5fce9a09d258b4978de2ddec921d7f2
brccabral <cabral.brc@gmail.com>
2021-10-21 00:42:28 -0700

    prepare form

---
+++ database.py
@@ -0,0 +1,63 @@
+import tkinter as tk
+import os
+import sqlite3
+from tkinter.constants import END
+
+root = tk.Tk()
+root.title("TKinter GUI")
+if os.name == "nt":
+    root.wm_iconbitmap(bitmap="python3.ico")
+else:
+    root.wm_iconbitmap(bitmap="@python3.xbm")
+root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
+root.geometry("400x400")
+
+conn = sqlite3.connect("address_book.db")
+
+c = conn.cursor()
+
+f_name_label = tk.Label(root, text="First name")
+f_name_label.grid(row=0, column=0)
+f_name = tk.Entry(root, width=30)
+f_name.grid(row=0, column=1, padx=20)
+
+l_name_label = tk.Label(root, text="Last name")
+l_name_label.grid(row=1, column=0)
+l_name = tk.Entry(root, width=30)
+l_name.grid(row=1, column=1)
+
+address_label = tk.Label(root, text="Address")
+address_label.grid(row=2, column=0)
+address = tk.Entry(root, width=30)
+address.grid(row=2, column=1)
+
+city_label = tk.Label(root, text="City")
+city_label.grid(row=3, column=0)
+city = tk.Entry(root, width=30)
+city.grid(row=3, column=1)
+
+state_label = tk.Label(root, text="State")
+state_label.grid(row=4, column=0)
+state = tk.Entry(root, width=30)
+state.grid(row=4, column=1)
+
+zipcode_label = tk.Label(root, text="Zipcode")
+zipcode_label.grid(row=5, column=0)
+zipcode = tk.Entry(root, width=30)
+zipcode.grid(row=5, column=1)
+
+def submit():
+    f_name.delete(0, END)
+    l_name.delete(0, END)
+    address.delete(0, END)
+    city.delete(0, END)
+    state.delete(0, END)
+    zipcode.delete(0, END)
+
+submit_btn = tk.Button(root, text="Add record to DB", command=submit)
+submit_btn.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+
+conn.commit()
+conn.close()
+
+root.mainloop()
