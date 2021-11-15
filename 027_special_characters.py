4b81637d29b695f26af6eb85ec6265787d138ff4
Bruno Cabral <cabral.brc@gmail.com>
2021-11-15 12:27:24 -0800

    unicode special characters

---
+++ 027_special_characters.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+import os
+
+root = tk.Tk()
+root.title("TKinter GUI")
+if os.name == "nt":
+    root.wm_iconbitmap(bitmap="python3.ico")
+else:
+    root.wm_iconbitmap(bitmap="@python3.xbm")
+root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
+root.geometry("400x400")
+
+my_label = tk.Label(root, text="42" + u'\u00b0', font=("Helvetica", 32)).pack(pady=10)
+
+
+
+root.mainloop()
