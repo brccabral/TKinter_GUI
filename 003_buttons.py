9812e3364b05f693eaf4b627af442bc73250014a
Bruno Cabral <cabral.brc@gmail.com>
2021-11-16 10:53:41 -0800

    buttons styles

--- 003_buttons.py
+++ 003_buttons.py
@@ -2,7 +2,7 @@ import tkinter as tk
 from tkinter.constants import DISABLED
 
 root = tk.Tk()
-root.geometry("400x400")
+root.geometry("400x900")
 root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 
 def myClick():
@@ -18,7 +18,11 @@ myButton3 = tk.Button(root, text="Click me!", padx=50, pady=25, command=myClick)
 myButton3.pack()
 myButton4 = tk.Button(root, text="Click me!", padx=50, pady=25, foreground="blue", background="green", highlightbackground="yellow", highlightcolor="white")
 myButton4.pack()
-myButton5 = tk.Button(root, text="Click me!", padx=50, pady=25, fg="white", bg="red", highlightbackground="magenta", highlightcolor="#888888")
+myButton5 = tk.Button(root, text="Click me!", padx=50, pady=25, fg="white", bg="red", highlightbackground="magenta", highlightcolor="#00FFFF", highlightthickness=5)
 myButton5.pack()
+
+buttons_styles = ["flat", "groove", "raised", "ridge", "solid", "sunken"]
+for s in buttons_styles:
+    tk.Button(root, text=f"Style {s}", relief=s, overrelief=s, font=('Helvetica',20)).pack()
 
 root.mainloop()
