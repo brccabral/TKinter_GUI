426035888d5df42d0781057f142538fa2f5850fd
Bruno Cabral <cabral.brc@gmail.com>
2021-11-17 12:01:25 -0800

    use config() to modify global widgets

--- 006_images.py
+++ 006_images.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-from tkinter.constants import DISABLED, E, SUNKEN, W
+from tkinter.constants import ACTIVE, DISABLED, E, SUNKEN, W
 # tkinter doesn't work with jpg
 from PIL import ImageTk, Image
 
@@ -29,42 +29,40 @@ image_list = [
 
 current_index = 0
 
-image_holder: tk.Label = tk.Label(image=image_list[current_index])
-image_holder.grid(row=0, column=0, columnspan=3)
+# use a Frame() with pack_propagate() to avoid shrinking window
+frame = tk.Frame(root, width=320, height=320)
+frame.grid(row=0, column=0, columnspan=3)
+frame.pack_propagate(False)
+image_holder = tk.Label(frame, image=image_list[current_index])
+image_holder.pack()
 
 # relief=SUNKEN - give a look of is going inside the screen
 # anchor=E - puts text aligned to the East
-status_label = tk.Label(root, text="Image 1 of " + str(len(image_list)), bd=1, relief=SUNKEN, anchor=E)
+status_label = tk.Label(root, text=f"Image {current_index+1} of {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
 # sticky=W+E - stretches the label on both directions West and East
 status_label.grid(row=2, column=0, columnspan=3, sticky=W+E)
 
+
 def image_loop(index):
-    global current_index, image_holder, button_back, button_next, status_label
+    global current_index
     current_index += index
-    
-    button_back = tk.Button(root, text="<<", command=lambda: image_loop(-1))
-    button_next = tk.Button(root, text=">>", command=lambda: image_loop(1))
+    button_back.config(state=ACTIVE)
+    button_next.config(state=ACTIVE)
 
     if current_index <= 0:
         current_index = 0
-        button_back = tk.Button(root, text="<<", command=lambda: image_loop(-1), state=DISABLED)
-        
+        button_back.config(state=DISABLED)
+
     if current_index >= len(image_list)-1:
         current_index = len(image_list) - 1
-        button_next = tk.Button(root, text=">>", command=lambda: image_loop(1), state=DISABLED)
+        button_next.config(state=DISABLED)
     
-    # need to grid button again
-    button_back.grid(row=1, column=0)
-    button_next.grid(row=1, column=2)
-    
-    # apply new image
-    image_holder.grid_forget()
-    image_holder = tk.Label(image=image_list[current_index])
-    image_holder.grid(row=0, column=0, columnspan=3)
+    # # apply new image
+    image_holder.config(image=image_list[current_index])
 
-    # change status text
-    status_label = tk.Label(root, text=f"Image {current_index+1} of {len(image_list)}", bd=1, relief=SUNKEN, anchor=E)
-    status_label.grid(row=2, column=0, columnspan=3, sticky=W+E)
+    # # change status text
+    status_label.config(text=f"Image {current_index+1} of {len(image_list)}")
+    status_label.grid_propagate(False)
 
 button_back = tk.Button(root, text="<<", command=lambda: image_loop(-1), state=DISABLED)
 button_quit = tk.Button(root, text="Exit program", command=root.quit)
