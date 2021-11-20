0cbcab0c098ef8b391ff9bcc12a54d81464276be
Bruno Cabral <cabral.brc@gmail.com>
2021-11-19 21:24:03 -0800

    textbox edit colors

--- 053_texteditor_app.py
+++ 053_texteditor_app.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-from tkinter import Button, Entry, Event, Frame, Label, Menu, PhotoImage, Scrollbar, filedialog
+from tkinter import Button, Event, Frame, Label, Menu, PhotoImage, Scrollbar, filedialog, colorchooser
 from tkinter.constants import BOTTOM, E, END, HORIZONTAL, INSERT, NONE, RIGHT, SEL, SEL_FIRST, SEL_LAST, SUNKEN, X, Y
 import tkinter.font as tkFont
 import logging
@@ -50,12 +50,16 @@ text_scroll.config(command=textbox.yview)
 horizontal_scroll.config(command=textbox.xview)
 
 
+original_font = tkFont.Font(root, textbox.cget('font'))
+
 bold_font = tkFont.Font(root, textbox.cget('font'))
 bold_font.configure(weight=tkFont.BOLD)
 italic_font = tkFont.Font(root, textbox.cget('font'))
 italic_font.configure(slant=tkFont.ITALIC)
 roman_font = tkFont.Font(root, textbox.cget('font'))
 roman_font.configure(slant=tkFont.ROMAN, size=14)
+
+color_font = tkFont.Font(root, textbox.cget('font'))
 
 textbox.tag_configure("bold", font=bold_font)
 textbox.tag_configure("italic", font=italic_font)
@@ -167,6 +171,7 @@ menu.add_cascade(label="Edit", menu=edit_menu)
 edit_menu.add_command(label="Cut", command=lambda: cut_txt(False), accelerator="Ctrl+x")
 edit_menu.add_command(label="Copy", command=lambda: copy_txt(False), accelerator="Ctrl+c")
 edit_menu.add_command(label="Paste", command=lambda: paste_txt(False), accelerator="Ctrl+v")
+edit_menu.add_separator()
 edit_menu.add_command(label="Undo", command=lambda: text_undo(False), accelerator="Ctrl+z")
 edit_menu.add_command(label="Redo", command=lambda: text_redo(False), accelerator="Ctrl+y")
 
@@ -186,21 +191,43 @@ def font_event(event: Event):
         change_font("roman")
     return "break" # this avoids default textbox behavior
 
-def change_font(tag_name):
+def change_font(tag_name, color_hex=None):
     if not textbox.tag_ranges(SEL):
         logging.debug(f'no text selected')
         return
     current_tags = textbox.tag_names(SEL_FIRST)
-    if tag_name in current_tags:
+    if tag_name == "colored":
+        textbox.tag_add(color_hex, SEL_FIRST, SEL_LAST)
+    elif tag_name in current_tags:
         textbox.tag_remove(tag_name, SEL_FIRST, SEL_LAST)
     else:
         textbox.tag_add(tag_name, SEL_FIRST, SEL_LAST)
+
+def color_text():
+    rgb, color_hex = colorchooser.askcolor()
+    if color_hex:
+        textbox.tag_configure(color_hex, foreground=color_hex)
+        change_font("colored", color_hex)
+
+def color_all_text():
+    rgb, color_hex = colorchooser.askcolor()
+    if color_hex:
+        textbox.config(foreground=color_hex)
+
+def color_background():
+    rgb, color_hex = colorchooser.askcolor()
+    if color_hex:
+        textbox.config(background=color_hex)
 
 font_menu = Menu(menu, tearoff=False)
 menu.add_cascade(label="Font", menu=font_menu)
 font_menu.add_command(label="Bold", command=lambda: change_font("bold"))
 font_menu.add_command(label="Italic", command=lambda: change_font("italic"))
 font_menu.add_command(label="Roman", command=lambda: change_font("roman"))
+font_menu.add_separator()
+font_menu.add_command(label="Color selected", command=color_text)
+font_menu.add_command(label="Color all", command=color_all_text)
+font_menu.add_command(label="Color background", command=color_background)
 
 textbox.bind("<Control-Key-b>", font_event)
 textbox.bind("<Control-Key-i>", font_event)
@@ -212,6 +239,12 @@ italic_button = Button(buttons_frame, font=italic_font, text="I", command=lambda
 italic_button.grid(row=0, column=1, padx=5)
 roman_button = Button(buttons_frame, font=roman_font, text="R", command=lambda: change_font("roman"))
 roman_button.grid(row=0, column=2, padx=5)
+undo_button = Button(buttons_frame, text="Undo", command=lambda: text_undo(False))
+undo_button.grid(row=0, column=3, padx=5)
+redo_button = Button(buttons_frame, text="Redo", command=lambda: text_redo(False))
+redo_button.grid(row=0, column=4, padx=5)
+color_text_button = Button(buttons_frame, text="Color selected", command=color_text)
+color_text_button.grid(row=0, column=5, padx=5)
 
 # PhotoImage can't open .jpg, use Pillow for .jpg
 image = PhotoImage(file="images/star.png")
@@ -220,8 +253,6 @@ def add_image():
     position = textbox.index(INSERT)
     textbox.image_create(position, image=image)
 
-def select_text():
-    selected = textbox.selection_get()
 
 status_bar = Label(root, text='Ready        ', relief=SUNKEN, anchor=E)
 status_bar.pack(fill=X, side=BOTTOM, ipady=5)
