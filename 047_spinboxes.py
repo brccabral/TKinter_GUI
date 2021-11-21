d69c29564fbcaf83f7654e3dba74b278615dac3e
Bruno Cabral <cabral.brc@gmail.com>
2021-11-20 23:34:11 -0800

    spinboxes set reset

--- 047_spinboxes.py
+++ 047_spinboxes.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import os
-from tkinter import Button, Label, font
+from tkinter import Button, Label, font, IntVar, StringVar
 
 root = tk.Tk()
 root.title("TKinter GUI")
@@ -11,14 +11,24 @@ else:
 root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 root.geometry("400x400")
 
+
 def get_values():
     label.config(text=f"{spin.get()=} {spin2.get()=}")
 
-spin = tk.Spinbox(root, from_=0, to=10, font=('Helvetica', 20), increment=2)
+
+init_spin_int = IntVar(root)
+init_spin_int.set(0)
+
+spin = tk.Spinbox(root, from_=0, to=50, font=('Helvetica', 20),
+                  increment=2, textvariable=init_spin_int)
 spin.pack(pady=10)
 
 names = ("John", "Tim", "Mary", "Tina")
-spin2 = tk.Spinbox(root, values=names, font=('Helvetica', 20), command=get_values)
+init_spin_str = StringVar(root)
+init_spin_str.set("John")
+
+spin2 = tk.Spinbox(root, values=names, font=('Helvetica', 20),
+                   command=get_values, textvariable=init_spin_str)
 spin2.pack(pady=10)
 
 button = Button(root, text="Submit", command=get_values)
@@ -26,6 +36,15 @@ button.pack(pady=10)
 
 label = Label(root, text="")
 label.pack(pady=10)
+
+
+def reset_values():
+    init_spin_int.set(0)
+    init_spin_str.set("John")
+
+
+reset_button = Button(root, text="Reset", command=reset_values)
+reset_button.pack(pady=10)
 
 
 root.mainloop()
