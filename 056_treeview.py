73278a1b035f9fc8a5918a2091292ef2af90c5b6
Bruno Cabral <cabral.brc@gmail.com>
2021-11-20 21:28:44 -0800

    treeview move up and down

--- 056_treeview.py
+++ 056_treeview.py
@@ -36,7 +36,8 @@ class TkinterApp:
         self.tree_scroll = Scrollbar(self.tree_frame)
         self.tree_scroll.pack(side=RIGHT, fill=Y)
 
-        self.tree = ttk.Treeview(self.tree_frame, yscrollcommand=self.tree_scroll.set)
+        self.tree = ttk.Treeview(
+            self.tree_frame, yscrollcommand=self.tree_scroll.set)
         # self.tree.config(selectmode=NONE) # disables selection
         # self.tree.config(selectmode=BROWSE) # allows select only one item
 
@@ -76,35 +77,72 @@ class TkinterApp:
         self.topping_entry = Entry(self.insert_frame)
         self.topping_entry.grid(row=1, column=2)
 
-        self.insert_button = Button(
-            self.root, text="Insert record", command=self.insert_record)
-        self.insert_button.pack(pady=10)
+        self.move_frame = Frame(self.root)
+        self.move_frame.pack(pady=10)
 
-        self.update_button = Button(
-            self.root, text="Update record", command=self.update_record)
-        self.update_button.pack(pady=10)
+        self.move_up_button = Button(
+            self.move_frame, text="Move up", command=self.move_up)
+        self.move_up_button.grid(row=0, column=0)
+
+        self.move_down_button = Button(
+            self.move_frame, text="Move down", command=self.move_down)
+        self.move_down_button.grid(row=0, column=1)
+
+        self.edit_buttons_frame = Frame(self.root)
+        self.edit_buttons_frame.pack(pady=10)
+
+        self.insert_button = Button(
+            self.edit_buttons_frame, text="Insert record", command=self.insert_record)
+        self.insert_button.grid(row=0, column=0, padx=5)
 
         self.select_button = Button(
-            self.root, text="Select record", command=self.select_record)
-        self.select_button.pack(pady=10)
+            self.edit_buttons_frame, text="Select record", command=self.select_record)
+        self.select_button.grid(row=0, column=1, padx=5)
+
+        self.update_button = Button(
+            self.edit_buttons_frame, text="Update record", command=self.update_record)
+        self.update_button.grid(row=0, column=2, padx=5)
+
+        self.remove_buttons_frame = Frame(self.root)
+        self.remove_buttons_frame.pack(pady=10)
 
         self.remove_all_button = Button(
-            self.root, text="Remove all", command=self.remove_all)
-        self.remove_all_button.pack(pady=10)
+            self.remove_buttons_frame, text="Remove all", command=self.remove_all)
+        self.remove_all_button.grid(row=0, column=0, padx=5)
 
         self.remove_one_button = Button(
-            self.root, text="Remove one", command=self.remove_one)
-        self.remove_one_button.pack(pady=10)
+            self.remove_buttons_frame, text="Remove one", command=self.remove_one)
+        self.remove_one_button.grid(row=0, column=1, padx=5)
 
         self.remove_many_button = Button(
-            self.root, text="Remove many", command=self.remove_many)
-        self.remove_many_button.pack(pady=10)
+            self.remove_buttons_frame, text="Remove many", command=self.remove_many)
+        self.remove_many_button.grid(row=0, column=2, padx=5)
 
         # self.tree.bind("<Double-1>", func=self.clicker)
         # the Button-1 gets called before focus(), so, nothing is selected yet
         # need to be ButtonRelease-1
         # self.tree.bind("<Button-1>", func=self.clicker)
         self.tree.bind("<ButtonRelease-1>", func=self.clicker)
+
+    def move_up(self):
+        rows = self.tree.selection()
+        if not rows:
+            logging.debug(f'nothing selected')
+            return
+
+        for row in rows:
+            self.tree.move(row, self.tree.parent(row), self.tree.index(row)-1)
+
+    def move_down(self):
+        rows = self.tree.selection()
+        if not rows:
+            logging.debug(f'nothing selected')
+            return
+
+        # need reversed() in case two selections are in sequence
+        for row in reversed(rows):
+            self.tree.move(row, self.tree.parent(row), self.tree.index(row)+1)
+
 
     def clicker(self, event: Event):
         self.select_record()
@@ -114,7 +152,7 @@ class TkinterApp:
         if not selected:
             logging.debug(f'no selection')
             return
-        
+
         if not self.name_entry.get() or not self.id_entry.get() or not self.topping_entry.get():
             messagebox.showerror("Treeview", "One or more entries is blank")
             return
@@ -134,7 +172,7 @@ class TkinterApp:
         if not selected:
             logging.debug(f'no selection')
             return
-        
+
         self.name_entry.delete(0, END)
         self.id_entry.delete(0, END)
         self.topping_entry.delete(0, END)
@@ -176,11 +214,12 @@ class TkinterApp:
     def insert_data(self, name: str, id: Union[str, int], topping: str, parent="", index=END, iid: int = None, text=""):
         if iid is None:
             iid = next(self.iid)
-        
+
         if parent:
             tag = self.tree.item(parent, 'tag')
         else:
-            tag = 'evenrow' if len(self.tree.get_children()) % 2 == 0 else 'oddrow'
+            tag = 'evenrow' if len(
+                self.tree.get_children()) % 2 == 0 else 'oddrow'
         self.tree.insert(parent=parent, index=index, iid=iid,
                          text=text, values=(name, id, topping), tags=tag)
 
