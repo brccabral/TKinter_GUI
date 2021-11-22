700caca88fbee6942a315e63f06558a9ab76f1b1
Bruno Cabral <cabral.brc@gmail.com>
2021-11-22 10:43:00 -0800

    focus tab order

--- 065_focus_tab_order.py
+++ 065_focus_tab_order.py
@@ -19,6 +19,28 @@ class TkinterApp:
         self.root.geometry(
             f"{width}x{heigth}+{screen_width//2-width//2}+{screen_height//2-heigth//2}")
 
+        # without "focus()", the definition order, not the pack(), is
+        # the focus order
+        self.white = tk.Entry(self.root, bg="white", font=('DejaVu Sans', 20))
+        self.red = tk.Entry(self.root, bg="red", font=('DejaVu Sans', 20))
+        self.blue = tk.Entry(self.root, bg="blue", font=('DejaVu Sans', 20))
+
+        self.white.pack(pady=10)
+        self.red.pack(pady=10)
+        self.blue.pack(pady=10)
+
+        # this sets the first focus, but the order is still the definition order
+        self.red.focus()
+        
+        # use liff() to set the order you need
+        self.set_focus_order()
+
+    def set_focus_order(self):
+        order = [self.red, self.white, self.blue]
+        w: tk.Widget
+        for w in order:
+            w.lift()
+
     def start(self):
         self.root.mainloop()
 
