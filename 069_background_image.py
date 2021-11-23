02b57a956cef494da4e9fe49e5b59e6690936dbe
Bruno Cabral <cabral.brc@gmail.com>
2021-11-22 22:48:52 -0800

    background image

--- 069_background_image.py
+++ 069_background_image.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 import os
-from tkinter import Button, Canvas, Frame, font
+from tkinter import Button, Canvas, Event, Frame
 from tkinter.constants import BOTH, NW
-
+from PIL import ImageTk, Image
+import logging
+logging.basicConfig(format='%(levelname)s - %(asctime)s - %(name)s - %(message)s', datefmt='%H:%M:%S', level=logging.DEBUG)
 
 class TkinterApp:
     def __init__(self, appname="TkinterApp", width=400, height=400):
@@ -19,7 +21,10 @@ class TkinterApp:
             self.root.wm_iconbitmap(bitmap="@python3.xbm")
         self.root.iconphoto(self.root._w, tk.PhotoImage(file='python3.png'))
 
-        self.bg_image = tk.PhotoImage(file="images/cam_logo.png")
+        self.bg_original = Image.open("images/cam_logo.png")
+        # self.bg_image = tk.PhotoImage(file="images/cam_logo.png")
+        self.bg_image = ImageTk.PhotoImage(self.bg_original)
+        
 
     def set_root_bg(self):
         self.label = tk.Label(self.root, image=self.bg_image)
@@ -42,7 +47,7 @@ class TkinterApp:
     def set_canvas_bg(self):
         self.canvas = Canvas(self.root, width=self.width, height=self.height)
         self.canvas.pack(fill=BOTH, expand=True)
-        self.canvas.create_image(0, 0, image=self.bg_image, anchor=NW)
+        self.canvas_bg = self.canvas.create_image(0, 0, image=self.bg_image, anchor=NW)
 
         self.canvas.create_text(200, 40, text="Welcome",
                                 font=('DejaVu Sans', 32), fill="red")
@@ -57,6 +62,14 @@ class TkinterApp:
             150, 150, anchor=NW, window=self.button2)
         self.button3_window = self.canvas.create_window(
             270, 150, anchor=NW, window=self.button3)
+
+        self.canvas.bind("<Configure>", self.resize)
+
+    def resize(self, event: Event):
+        logging.debug(f'resize {event=}')
+        resized = self.bg_original.resize((event.width, event.height), Image.ANTIALIAS)
+        self.bg_image = ImageTk.PhotoImage(resized)
+        self.canvas.itemconfig(self.canvas_bg, image=self.bg_image)
 
     def start(self):
         self.center_window()
