f80a8c200d777f00f34e7942b31870a7d5167f4e
Bruno Cabral <cabral.brc@gmail.com>
2021-11-26 23:25:44 -0800

    use a variable to control Progressbar

--- 040_progress_bar.py
+++ 040_progress_bar.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import Button, Label, ttk
+from tkinter import Button, DoubleVar, Label, ttk
 import os
 import time
 from tkinter.constants import HORIZONTAL
@@ -13,32 +13,55 @@ else:
 root.iconphoto(root._w, tk.PhotoImage(file='python3.png'))
 root.geometry("600x400")
 
-my_progress = ttk.Progressbar(root, value=0, orient=HORIZONTAL, length=300, mode="determinate")
+my_progress = ttk.Progressbar(
+    root, value=0, orient=HORIZONTAL, length=300, mode="determinate")
 my_progress.pack(pady=10)
 
-my_progress2 = ttk.Progressbar(root, value=0, orient=HORIZONTAL, length=300, mode="indeterminate")
+my_progress2 = ttk.Progressbar(
+    root, value=0, orient=HORIZONTAL, length=300, mode="indeterminate")
 my_progress2.pack(pady=10)
+
+my_progress3 = ttk.Progressbar(
+    root, value=0, orient=HORIZONTAL, length=300, maximum=20.0, mode="determinate")
+# use a variable to control Progressbar
+my_progress3.pack(pady=10)
+progress_value = DoubleVar()
+my_progress3.config(variable=progress_value)
+
 
 def step_progress():
     my_progress['value'] += 20
     my_progress2['value'] += 20
+    progress_value.set(progress_value.get()+1.5)
     my_label.config(text=my_progress['value'])
+
 
 def auto_progress():
     for x in range(5):
         my_progress['value'] += 20
         my_progress2['value'] += 20
+        progress_value.set(progress_value.get()+1.5)
         my_label.config(text=my_progress['value'])
         root.update_idletasks()
         time.sleep(1)
+
 
 def start_progress():
     my_progress.start(20)
     my_progress2.start(20)
 
+    # cancel = one step
+    # idle = super fast
+    # info = doesn't work
+    # integer = the greater the slower
+    my_progress3.start(200)
+
+
 def stop_progress():
     my_progress.stop()
     my_progress2.stop()
+    my_progress3.stop()
+
 
 my_button = Button(root, text="Progress Step", command=step_progress)
 my_button.pack(pady=10)
